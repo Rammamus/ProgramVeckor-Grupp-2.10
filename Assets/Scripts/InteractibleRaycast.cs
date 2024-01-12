ca574e6fbc9425edbf46ba857f38703ee50df09a
Klaus Schwab <120007795+Kakalladis@users.noreply.github.com>
2024-01-12 14:40:57 +0100

    lite saker

--- Assets/Scripts/InteractibleRaycast.cs
+++ Assets/Scripts/InteractibleRaycast.cs
@@ -6,6 +6,7 @@ public class InteractibleRaycast : MonoBehaviour
 {
     public RaycastHit hit;
 
+    GameObject equippedItem;
     bool holdingSomething = false;
     [SerializeField] KeyCode interactkey = KeyCode.F;
     [SerializeField] float raycastRange;
@@ -21,25 +22,45 @@ public class InteractibleRaycast : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   //Sätter Vektorn fwd till framåt från kameran -Filip
+    {
+        print(holdingSomething);
+        //Sätter Vektorn fwd till framåt från kameran -Filip
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
         //Känner efter om Raycasten träffar någonting raycastRange units framför objektet och överför informationen från det träffade objektet till raycastHit-variabeln hit - Filip
         if (Physics.Raycast(transform.position, fwd, out hit, raycastRange))
         {
 
-
-            //Kollar om det träffade gameObjectet har taggen Interactable och aktiverar "Press X to interact" texten -Filip
+            print("Interact");
+            //Kollar om det träffade gameObjectet har taggen Interactable och aktiverar "Press F to interact" texten -Filip
             if (hit.transform.gameObject.CompareTag("Interactable") && holdingSomething == false)
-            {
+             {
                 interractText.SetActive(true);
                 if (Input.GetKey(interactkey))
                 {
                     hit.transform.parent = gameObject.transform;
-                    
+                    holdingSomething = true;
                     
                 }
+
             }
+
+            else if (holdingSomething == true)
+            {
+                interractText.SetActive(false);
+                if (Input.GetKey(interactkey))
+                {
+                    
+                    holdingSomething = false;
+
+
+                }
+                
+            }
+
+
+
+        }
             else
             {
                 interractText.SetActive(false);
@@ -52,4 +73,4 @@ public class InteractibleRaycast : MonoBehaviour
             
     }
     
-}
+
