8b01d6bf5a197ac72c454fae41eb5a2cb47c3f0d
williamahlen <120007845+williamahlen@users.noreply.github.com>
2024-01-12 15:27:32 +0100

    Audio

--- Assets/Scripts/musicsettings.cs
+++ Assets/Scripts/musicsettings.cs
@@ -8,7 +8,39 @@ public class musicsettings : MonoBehaviour
 
 
     [SerializeField] private AudioMixer mymixer;
-    [SerializeField] private Slider muxicslider;
-
+    [SerializeField] private Slider musicslider;
+    [SerializeField] private Slider SFXslider;
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicvolume"))
+        {
+            loadvolume();
+        }
+        else
+        {
+            setmusicvolume();
+            setSFXvolume();
+        }
+        
+    }
+    public void setmusicvolume()
+    {
+        float volume = musicslider.value;
+        mymixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.GetFloat("musicvolume");
+    }
+    public void setSFXvolume()
+    {
+        float volume = SFXslider.value;
+        mymixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.GetFloat("SFXvolume");
+    }
+    private void loadvolume()
+    {
+        musicslider.value = PlayerPrefs.GetFloat("musicvolume");
+        SFXslider.value = PlayerPrefs.GetFloat("SFXvolume");
+        setmusicvolume();
+        setSFXvolume();
+    }
 
 }
