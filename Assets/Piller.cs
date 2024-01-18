ac3a95052ab34208682fc07642f3fac32faaa797
Klaus Schwab <120007795+Kakalladis@users.noreply.github.com>
2024-01-18 14:56:38 +0100

    Piller

---
+++ Assets/Piller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piller : MonoBehaviour
+{
+    public int remainingPills;
+    [SerializeField] KeyCode pillerSmaskarn;
+
+    void takePill()
+    {
+        remainingPills -= 1;
+        print(remainingPills + " Remaining Pills");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.parent != null)
+        {
+            if (Input.GetKeyDown(pillerSmaskarn) && remainingPills > 0)
+            {
+                takePill();
+            }
+            else if (Input.GetKeyDown(pillerSmaskarn) && remainingPills <= 0)
+            {
+
+            }
+        }
+       
+    }
+}
