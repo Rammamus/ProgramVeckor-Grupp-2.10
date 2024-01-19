079fa2272274349d82d8bcba7ae50f8cfc2083cc
Rammamus <adrian.folin@elev.ga.lbs.se>
2024-01-19 10:18:25 +0100

    WIN WIN WIN WIN WIN WIN

--- Assets/Scripts/Interacting/HomeDoor.cs
+++ Assets/Scripts/Interacting/HomeDoor.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class HomeDoor : MonoBehaviour
 {
     [SerializeField] GameObject door;
+    [SerializeField] GameObject text;
+
+    private void Start()
+    {
+        text.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
         //I have been awake for more than 24 hours and this is all the logic my brain can produce - Adrian
-        if (FindObjectOfType<InteractibleRaycast>().hit.transform == door)
+        if (FindObjectOfType<InteractibleRaycast>().hit.transform.gameObject == door)
         {
-            Debug.Log("gets hit");
+            text.SetActive(true);
             if (Input.GetKeyDown(KeyBinds.interact))
             {
                 FindObjectOfType<SceneManage>().GoToWork();
-                Debug.Log("GoToWork");
             }
+        }
+        else
+        {
+            text.SetActive(false);
         }
     }
 }
