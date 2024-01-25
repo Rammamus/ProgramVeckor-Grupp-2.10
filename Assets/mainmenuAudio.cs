67941517c45df2fc662c9d5ea7fda8109d72aa25
Klaus Schwab <120007795+Kakalladis@users.noreply.github.com>
2024-01-25 11:12:55 +0100

    Musiken i main menu är fixad

---
+++ Assets/mainmenuAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenuAudio : MonoBehaviour
+{
+    AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager.playMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
